60c13e8b6ab25cfc817d342a2d08c48e91a9874d
Pascaldawn <pascalbernard793@gmail.com>
2022-12-11 07:59:11 -0800

    printf code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main ~ c hello world;
+ *
+ * return: always (0);
+ */
+ int main(void)
+ {
+ 	puts("\nProgramming is like building a multilingual puzzle");
+
+	return (0);
+	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main ~ this is a  hello printf c code;
+ *
+ * return: always 0;
+ */
+int main(void)	/* main: printf code */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
