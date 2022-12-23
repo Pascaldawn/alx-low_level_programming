bb3d4afb843cd070d3583a96bd5a5943a5b2f8d6
Pascaldawn <pascalbernard793@gmail.com>
2022-12-23 05:57:51 -0800

    va types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints variable size types
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of long int: %zu bytes\n", sizeof(long int));
+	printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of float: %zu bytes\n", sizeof(float));
+	return (0);
+}
